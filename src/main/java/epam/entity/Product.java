2363a56e83c0524e6fdf92a183a93e883b682019
Vadim Pogorelkin <vadim_pogorelkin@epam.com>
2019-07-01 17:57:46 +0400

    enhance

--- src/main/java/epam/entity/Product.java
+++ src/main/java/epam/entity/Product.java
@@ -1,10 +1,14 @@
 package epam.entity;
 
+import javax.xml.bind.annotation.*;
 import java.time.LocalDate;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Product {
+
+    @XmlAttribute
  private String manufacturer;
- private String model; //2chararcters & 3 numbers
+ private String model;
  private LocalDate dateOfManufacture;
  private String color;
  private Integer price;
@@ -19,5 +23,51 @@ public class Product {
         this.amount = amount;
     }
 
+    public String getManufacturer() {
+        return manufacturer;
+    }
 
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public LocalDate getDateOfManufacture() {
+        return dateOfManufacture;
+    }
+
+    public void setDateOfManufacture(LocalDate dateOfManufacture) {
+        this.dateOfManufacture = dateOfManufacture;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
 }
